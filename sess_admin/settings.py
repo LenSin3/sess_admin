8e6e0e4e0303913ae0a878647975ecc1031b71d2
LenSin3 <lenpk3@yahoo.co.uk>
2025-03-03 23:20:19 -0800

    changed url to public

--- sess_admin/settings.py
+++ sess_admin/settings.py
@@ -124,7 +124,7 @@ else:
     # Railway PostgreSQL configuration using DATABASE_URL
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
+            default=os.environ.get('DATABASE_PUBLIC_URL'),
             conn_max_age=600,  # Optimize connection pooling
             engine='django.db.backends.postgresql'
         )
