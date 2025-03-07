ecc3136a749c092f17c97786525fc39ce48422f5
LenSin3 <lenpk3@yahoo.co.uk>
2025-03-07 00:20:09 -0800

    using db public url

--- sess_admin/settings.py
+++ sess_admin/settings.py
@@ -163,7 +163,7 @@ else:
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_PUBLIC_URL'),
         conn_max_age=600,  # Optimize connection pooling
         engine='django.db.backends.postgresql'
     )
